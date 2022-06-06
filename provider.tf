50152904ccd8ec5e3d036bf8d2807ad11efc9ecb
Kyle Alderson <kyle.alderson91@gmail.com>
2022-06-06 14:04:18 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+  cloud {
+    organization = "kylesbx"
+    workspaces {
+      tags = ["env:#{environment}#", "app:tst-app-kyle"]
+    }
+  }
 }
 
 # Configure the AWS Provider

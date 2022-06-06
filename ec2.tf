801c7ace65674ad4bcbe8a7389d62ee3946caf66
Kyle Alderson <kyle.alderson91@gmail.com>
2022-06-06 14:21:52 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,9 @@
-resource "aws_instance" "test-ec2" {
+resource "aws_instance" "test-ec2-1" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "test-ec2-2" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }

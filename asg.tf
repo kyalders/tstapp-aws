09e00aa072f84058bba55eef6b6f4ecb09d1f2f3
Kyle Alderson <kyle.alderson91@gmail.com>
2022-11-26 11:43:44 -0500

    testing asg

--- asg.tf
+++ asg.tf
@@ -2,7 +2,7 @@ resource "aws_launch_template" "web_template" {
   name_prefix   = "web"
   image_id      = "ami-0beaa649c482330f7"
   instance_type = "t2.small"
-  user_date = filebase64("apache.sh")
+  user_data = filebase64("apache.sh")
 }
 
 resource "aws_autoscaling_group" "web_asg" {

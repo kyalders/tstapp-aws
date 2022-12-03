dbb063f7d27295519fe9378ae62744319bdd5087
Kyle Alderson <kyle.alderson91@gmail.com>
2022-12-03 14:37:53 -0600

    Create subnet.tf

---
+++ subnet.tf
@@ -0,0 +1,20 @@
+# Creating 1st subnet 
+resource "aws_subnet" "tstapp-subnet1" {
+  vpc_id                  = "${aws_vpc.tstappvpc.id}"
+  cidr_block             = "${var.subnet_cidr}"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+tags = {
+    Name = "Testapp subnet 1"
+  }
+}
+# Creating 2nd subnet 
+resource "aws_subnet" "tstapp-subnet-2" {
+  vpc_id                  = "${aws_vpc.tstappvpc.id}"
+  cidr_block             = "${var.subnet1_cidr}"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1b"
+tags = {
+    Name = "TSTapp subnet 2"
+  }
+}

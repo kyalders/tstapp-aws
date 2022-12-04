d8c01bc7cacc667fe2eb1ad6f47483943286084e
Kyle Alderson <kyle.alderson91@gmail.com>
2022-12-03 20:18:26 -0600

    Update launch_config.tf

--- launch_config.tf
+++ launch_config.tf
@@ -2,7 +2,7 @@ resource "aws_launch_configuration" "web" {
   name_prefix = "web-"
 image_id = "ami-087c17d1fe0178315" 
   instance_type = "t2.micro"
-  key_name = "tests"
+  key_name = "tstapp"
 security_groups = [ "${aws_security_group.tstapp-ec2-sg.id}" ]
   associate_public_ip_address = true
   user_data = "${file("data.sh")}"

6ef9e500f5313c4109f978e92f6da33952773913
Kyle Alderson <kyle.alderson91@gmail.com>
2022-12-08 18:29:35 -0500

    Create sg_rds.tf

---
+++ sg_rds.tf
@@ -0,0 +1,13 @@
+resource "aws_security_group" "tstapp-rds-sg" {
+  name        = "Test App RDS Security Group"
+  description = "Test App SG Module for RDS"
+  vpc_id = "${aws_vpc.tstappvpc.id}"
+# Inbound Rules
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  

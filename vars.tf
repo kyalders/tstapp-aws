c647875396e164076e7508fb1cf8120a7e7c6c29
Kyle Alderson <kyle.alderson91@gmail.com>
2022-12-08 18:46:26 -0500

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -24,7 +24,7 @@ variable "subnet2_cidr" {
   default = "10.0.2.0/24"
 }
 # Defining CIDR Block for RDS Subnet
-variable "subnet2_cidr" {
+variable "subnet_rds_cidr" {
   default = "10.0.3.0/24"
 }
 

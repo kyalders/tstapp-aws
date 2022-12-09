936c38fd7ed0e68952c6d83dbd3b47ceaaa00a31
Kyle Alderson <kyle.alderson91@gmail.com>
2022-12-08 19:35:20 -0500

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -10,17 +10,17 @@ data "aws_kms_secret" "rds-secret" {
 }
 
 resource "aws_db_instance" "my_test_mysql" {
-  allocated_storage           = 50
+  allocated_storage           = 20
   storage_type                = "gp2"
   engine                      = "mysql"
   engine_version              = "8.0"
   instance_class              = "${var.db_instance}"
   name                        = "myrdstestmysql"
   username                    = "admin"
-  password                    = "${data.aws_kms_secret.rds-secret.name}"
+  password                    = "${data.aws_kms_secret.rds-secret.master_password}"
   parameter_group_name        = "default.mysql8.0"
   db_subnet_group_name        = "${aws_db_subnet_group.rds-private-subnet.name}"
-  vpc_security_group_ids      = ["${aws_security_group.tstapp-rds-sg.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.rds-sg.id}"]
   allow_major_version_upgrade = true
   auto_minor_version_upgrade  = true
   backup_retention_period     = 35

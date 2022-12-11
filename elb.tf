b4c702056c7d1e6b185050ac30f954f438229a77
Kyle Alderson <kyle.alderson91@gmail.com>
2022-12-11 16:18:00 -0500

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -8,13 +8,13 @@ resource "aws_elb" "web_elb" {
     "${aws_subnet.tstapp-subnet2.id}"
   ]
 cross_zone_load_balancing   = true
-health_check {
+/*health_check {
     healthy_threshold = 2
     unhealthy_threshold = 2
     timeout = 3
     interval = 30
     target = "HTTP:80/"
-  }
+  }*/
 listener {
     lb_port = 80
     lb_protocol = "http"

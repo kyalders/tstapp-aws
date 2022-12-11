2bac6cd3a045d907b1d3553cb63ad91f57324861
Kyle Alderson <kyle.alderson91@gmail.com>
2022-12-11 15:16:13 -0500

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,24 +1,13 @@
-# Note, must comment this whole file upon initial build. Need to enable key after it's built and then run:
-# aws kms encrypt --key-id <kms key id> --plaintext admin123 --output text --query CiphertextBlob
-# in aws console
-/*
-data "aws_kms_secret" "rds-secret" {
-  secret {
-    name = "master_password"
-    payload = "AQICAHiSb3p1R70gmxMrrQNK4gw26AT/ZITG3zWiHDWtJApAhgGfrsWnHk1A7J063/WvqBVLAAAAZDBiBgkqhkiG9w0BBwagVTBTAgEAME4GCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMEJnt+f6kiDjrStJVAgEQgCGnZFaG10fYaA5MNblxe+TQxDNYRRGX6zG4JR/6/zzjQ5Q="
-  }
-}
-
 resource "aws_db_instance" "my_test_mysql" {
   allocated_storage           = 20
   storage_type                = "gp2"
   engine                      = "mysql"
-  engine_version              = "8.0"
+  engine_version              = "5.7"
   instance_class              = "${var.db_instance}"
   name                        = "myrdstestmysql"
   username                    = "admin"
-  password                    = "${data.aws_kms_secret.rds-secret.master_password}"
-  parameter_group_name        = "default.mysql8.0"
+  password                    = "admin123"
+  parameter_group_name        = "default.mysql5.7"
   db_subnet_group_name        = "${aws_db_subnet_group.rds-private-subnet.name}"
   vpc_security_group_ids      = ["${aws_security_group.rds-sg.id}"]
   allow_major_version_upgrade = true
@@ -29,4 +18,3 @@ resource "aws_db_instance" "my_test_mysql" {
   multi_az                    = true
   skip_final_snapshot         = true
 }
-*/

51683a0832a43062783bc57c028641e4253fdcdb
Kyle Alderson <kyle.alderson91@gmail.com>
2022-12-13 14:26:38 -0500

    updating asg template

--- data.sh
+++ data.sh
@@ -4,6 +4,8 @@ sudo yum install httpd -y
 sudo yum install git -y
 sudo yum install php -y
 sudo yum install mysql -y
+sudo amazon-linux-extras enable php7.4 -y
+sudo yum install php-cli php-pdo php-fpm php-json php-mysqlnd -y
 sudo yum install php-gd -y
 sudo yum install php-pear -y
 sudo yum install php-mbstring -y
@@ -14,8 +16,6 @@ sudo yum install php-intl -y
 sudo yum install php-zip -y
 sudo yum install php-zts -y
 sudo yum install php-xml -y
-sudo amazon-linux-extras enable php7.4 -y
-sudo yum install php-cli php-pdo php-fpm php-json php-mysqlnd -y
 sudo mkdir /var/www/moodledata
 sudo chown apache:apache /var/www/moodledata
 sudo cd /var/www/html

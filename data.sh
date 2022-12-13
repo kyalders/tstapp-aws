8549c2a57c1313e46172737e7a8bbef333377741
Kyle Alderson <kyle.alderson91@gmail.com>
2022-12-12 23:10:20 -0500

    updating asg template

--- data.sh
+++ data.sh
@@ -14,7 +14,7 @@ sudo yum install php-zip -y
 sudo yum install php-zts -y
 sudo yum install php-xml -y
 sudo mkdir /var/www/moodledata
-sudo mkdir /var/www/moodledata
+sudo mkdir /var/www/html/moodle
 sudo chown apache:apache /var/www/moodledata
 sudo chown -R apache:apache /var/www/html/moodle
 sudo cd /var/www/html

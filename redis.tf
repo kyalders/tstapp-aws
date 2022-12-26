4a4b862087c13eb2acac248af464c725af65f0b8
Kyle Alderson <kyle.alderson91@gmail.com>
2022-12-26 11:11:20 -0500

    Update redis.tf

--- redis.tf
+++ redis.tf
@@ -3,7 +3,6 @@ resource "aws_elasticache_cluster" "tstapp_redis_cluster" {
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  availability_zones   = ["us-east-1a"]
   port                 = 6379
   parameter_group_name = "default.redis5.0"
   security_group_ids   = [aws_security_group.tstapp_redis_security_group.id]

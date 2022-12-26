06923be25d1f5189407c6dcf7f5084e6e01e1e13
Kyle Alderson <kyle.alderson91@gmail.com>
2022-12-26 11:00:56 -0500

    Create redis.tf

---
+++ redis.tf
@@ -0,0 +1,11 @@
+resource "aws_elasticache_cluster" "tstapp_redis_cluster" {
+  cluster_id          = "tstapp-redis-cluster"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  availability_zones   = ["us-east-1a"]
+  port                 = 6379
+  parameter_group_name = "default.redis5.0"
+  security_group_ids   = [aws_security_group.redis_security_group.id]
+  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
+}

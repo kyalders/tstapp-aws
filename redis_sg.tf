a913afb8dbd5c9b55cee9f419bbca4e04e308636
Kyle Alderson <kyle.alderson91@gmail.com>
2022-12-26 11:08:53 -0500

    Create redis_sg.tf

---
+++ redis_sg.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "tstapp_redis_security_group" {
+  name        = "tstapp-redis-security-group"
+  description = "Security group for my Redis cluster"
+  vpc_id      = aws_vpc.tstappvpc.id
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}

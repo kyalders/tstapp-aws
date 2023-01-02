5d0556de78e944605400da0503544c2cf59ec626
Kyle Alderson <kyle.alderson91@gmail.com>
2023-01-02 16:35:48 -0500

    modified parameter group for redis cluster

--- redis.tf
+++ redis.tf
@@ -4,7 +4,8 @@ resource "aws_elasticache_cluster" "tstapp_redis_cluster" {
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
   port                 = 6379
-  parameter_group_name = "redis5.0"
+  engine_version       = "5.0.6"
+  parameter_group_name = aws_elasticache_parameter_group.tst_app_redis_param_group.name
   security_group_ids   = [aws_security_group.tstapp_redis_security_group.id]
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
 }

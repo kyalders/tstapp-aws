9bd3027f2b9bd91fc74d1f394c2d91ed838b4bdd
Kyle Alderson <kyle.alderson91@gmail.com>
2023-01-02 18:36:37 -0500

    modifying user data

--- alb.tf
+++ alb.tf
@@ -26,4 +26,11 @@ resource "aws_lb_target_group" "tstapp_target_group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.tstapp-vpc.id
+
+  health_check {
+    path = "/moodle"
+    interval = 30
+    timeout = 5
+    healthy_threshold = 2
+  }
 }

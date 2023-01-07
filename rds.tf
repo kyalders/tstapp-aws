2509e1bbb20fa6929af8e9b306876255919fe8cc
kyalders <kyle.alderson91@gmail.com>
2023-01-07 15:27:22 -0500

    testing automated config.php

--- rds.tf
+++ rds.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "my_test_mysql" {
   engine                      = "mysql"
   engine_version              = "5.7"
   instance_class              = var.db_instance
-  db_name                     = "default"
+  db_name                     = "admindb"
   username                    = "admin"
   password                    = var.rds_password
   parameter_group_name        = "default.mysql5.7"

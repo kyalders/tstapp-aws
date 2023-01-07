ffbcda0699e998411fe1fc829ba607436853c98f
kyalders <kyle.alderson91@gmail.com>
2023-01-07 16:38:39 -0500

    testing automated config.php... again

--- data.sh
+++ data.sh
@@ -20,42 +20,43 @@ sudo mkdir /var/www/moodledata /var/www/html/moodle
 sudo chown apache:apache /var/www/moodledata /var/www/html/moodle
 sudo git clone https://github.com/moodle/moodle.git /var/www/html/moodle
 ## Make config.php ##
-set +x
-echo "<?php  // Moodle configuration file
+set -x
 
-unset($CFG);
-global $CFG;
-$CFG = new stdClass();
+cat <<EOT >> /var/www/html/moodle/config.php
+\<?php  // Moodle configuration file
 
-$CFG->dbtype    = 'mysqli';
-$CFG->dblibrary = 'native';
-$CFG->dbhost    = '${moodledbhost}';
-$CFG->dbname    = 'moodle';
-$CFG->dbuser    = '${moodledbuser}';
-$CFG->dbpass    = '${moodledbpw}';
-$CFG->prefix    = 'mdl_';
-$CFG->dboptions = array (
+\unset($CFG);
+\global $CFG;
+\$CFG = new stdClass();
+
+\$CFG->dbtype    = 'mysqli';
+\$CFG->dblibrary = 'native';
+\$CFG->dbhost    = '${moodledbhost}';
+\$CFG->dbname    = 'moodle';
+\$CFG->dbuser    = '${moodledbuser}';
+\$CFG->dbpass    = '${moodledbpw}';
+\$CFG->prefix    = 'mdl_';
+\$CFG->dboptions = array (
   'dbpersist' => 0,
   'dbport' => 3306,
   'dbsocket' => '',
   'dbcollation' => 'utf8mb4_unicode_ci',
 );
 
-$CFG->wwwroot   = '${moodlealb}/moodle';
-$CFG->dataroot  = '/var/www/moodledata';
-$CFG->admin     = 'admin';
+\$CFG->wwwroot   = '${moodlealb}/moodle';
+\$CFG->dataroot  = '/var/www/moodledata';
+\$CFG->admin     = 'admin';
 
-$CFG->directorypermissions = 0777;
+\$CFG->directorypermissions = 0777;
 
-$CFG->session_handler_class = '\core\session\redis';
-$CFG->session_redis_host = '${redis_endpoint}';
-$CFG->session_redis_port = 6379;
+\$CFG->session_handler_class = '\core\session\redis';
+\$CFG->session_redis_host = '${redis_endpoint}';
+\$CFG->session_redis_port = 6379;
 
 require_once(__DIR__ . '/lib/setup.php');
 
 // There is no php closing tag in this file,
 // it is intentional because it prevents trailing whitespace problems!
-set -x " > /var/www/html/moodle/config.php
-
+EOT
 sudo systemctl start httpd
 sudo systemctl enable httpd

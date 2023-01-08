614a589ea0e50143c7698c2fd5d1f954a7eb4d97
kyalders <kyle.alderson91@gmail.com>
2023-01-08 00:08:14 -0500

    adding ansible host and remote-exec for sql

--- ansible.tf
+++ ansible.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "ansible_host" {
-  ami           = "ami-0b5eea76982371e91"
-  instance_type = "t2.micro"
-  key_name      = "tstapp"
+  ami                    = "ami-0b5eea76982371e91"
+  instance_type          = "t2.micro"
+  key_name               = "tstapp"
   vpc_security_group_ids = ["${aws_security_group.tstapp-ec2-sg.id}"]
-  subnet_id = "${aws_subnet.tstapp-subnet1.id}"
+  subnet_id              = aws_subnet.tstapp-subnet1.id
 
   # Add a root volume
   root_block_device {
@@ -11,7 +11,7 @@ resource "aws_instance" "ansible_host" {
     volume_size           = 15
     delete_on_termination = true
   }
-  
+
   # Add a provisioner block to install ansible on the EC2 instance
   provisioner "remote-exec" {
     inline = [

e853b98d8198939462cce5604b2fc85498aaaf93
kyalders <kyle.alderson91@gmail.com>
2023-01-09 20:56:58 -0500

    adding ansible host and remote-exec for sql

--- s3.tf
+++ s3.tf
@@ -8,16 +8,10 @@ resource "aws_s3_bucket_acl" "moodle-bucket-acl" {
   acl    = "private"
 }
 
-
-data "template_file" "moodle-setup" {
-  template = file("moodle_setup.sql")
-
-}
-
 resource "aws_s3_object" "moodle-config" {
   bucket = aws_s3_bucket.moodle-bucket.id
   key    = "./moodle_setup.sql"
-  source = data.template_file.moodle-setup.rendered
+  source = "./moodle_setup.sql"
   depends_on = [
     aws_s3_bucket.moodle-bucket
   ]

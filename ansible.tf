ef1ca1cdaf7f4612d1221bc915e901e1e411da88
kyalders <kyle.alderson91@gmail.com>
2023-01-09 23:32:40 -0500

    testing sql mod

--- ansible.tf
+++ ansible.tf
@@ -1,3 +1,25 @@
+data "template_file" "ansible_config"{
+  template = <<EOF
+#!/bin/bash
+
+# Install Ansible
+sudo yum update -y
+sudo yum install mysql -y
+sudo amazon-linux-extras install ansible2 -y
+
+export MYSQL_MOODLE_PW=my_sql_moodlepw
+export AWS_ACCESS_KEY_ID=access_key
+export AWS_SECRET_ACCESS_KEY=secret_access
+
+
+# Download the SQL script from the S3 bucket
+aws s3 cp s3://${aws_s3_bucket.moodle-bucket.bucket}/moodle_setup.sql . --access-key AWS_ACCESS_KEY_ID --secret-key AWS_SECRET_ACCESS_KEY
+
+# Connect to RDS instance and execute SQL script
+mysql -h ${aws_db_instance.my_test_mysql.address} -u admin -p ${var.rds_password} < moodle_setup.sql
+EOF
+}
+
 resource "aws_instance" "ansible_host" {
   ami                    = "ami-0b5eea76982371e91"
   instance_type          = "t2.micro"
@@ -18,24 +40,5 @@ resource "aws_instance" "ansible_host" {
     secret_access = local.AWS_SECRET_ACCESS_KEY
   }
 
-    user_data = <<EOF
-#!/bin/bash
-
-# Install Ansible
-sudo yum update -y
-sudo yum install mysql -y
-sudo amazon-linux-extras install ansible2 -y
-
-export MYSQL_MOODLE_PW=my_sql_moodlepw
-export AWS_ACCESS_KEY_ID=access_key
-export AWS_SECRET_ACCESS_KEY=secret_access
-
-
-# Download the SQL script from the S3 bucket
-aws s3 cp s3://${aws_s3_bucket.moodle-bucket.bucket}/moodle_setup.sql . --access-key AWS_ACCESS_KEY_ID --secret-key AWS_SECRET_ACCESS_KEY
-
-# Connect to RDS instance and execute SQL script
-mysql -h ${aws_db_instance.my_test_mysql.address} -u admin -p ${var.rds_password} < moodle_setup.sql
-
-EOF
+    user_data = data.template_file.ansible_config.rendered
 }

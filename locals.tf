a806c8dc7126e4f5c5149100c8c6db6367605853
kyalders <kyle.alderson91@gmail.com>
2023-01-09 23:40:27 -0500

    testing sql mod

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-    AWS_ACCESS_KEY_ID = lookup(var.access_key, terraform.workspace)
-    AWS_SECRET_ACCESS_KEY = lookup(var.secret_access, terraform.workspace)
-    MYSQL_MODDLE_PW = lookup(var.mysql_moodle_pw, terraform.workspace)
+    AWS_ACCESS_KEY_ID = lookup(var.AWS_ACCESS_KEY_ID, terraform.workspace)
+    AWS_SECRET_ACCESS_KEY = lookup(var.AWS_SECRET_ACCESS_KEY, terraform.workspace)
+    MYSQL_MODDLE_PW = lookup(var.MYSQL_MODDLE_PW, terraform.workspace)
 }

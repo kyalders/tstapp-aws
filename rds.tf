63a86cb64aaf2379e005df3d9ac0c234ee311c68
Kyle Alderson <kyle.alderson91@gmail.com>
2023-01-13 16:33:02 -0500

    testing db config

--- rds.tf
+++ rds.tf
@@ -2,12 +2,11 @@ resource "aws_db_instance" "my_test_mysql" {
   allocated_storage           = 20
   storage_type                = "gp2"
   engine                      = "mysql"
-  engine_version              = "8.0"
+  engine_version              = "5.7"
   instance_class              = var.db_instance
   db_name                     = var.moodledbname
   username                    = var.moodledbuser
   password                    = var.rds_password
-  character_set_name          = "utf8"
   db_subnet_group_name        = aws_db_subnet_group.rds-private-subnet.name
   vpc_security_group_ids      = ["${aws_security_group.tstapp-rds-sg.id}"]
   allow_major_version_upgrade = true

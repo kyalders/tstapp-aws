d7371d16de0d64345f4efcc38b7063a16ddb6a20
kyalders <kyle.alderson91@gmail.com>
2023-01-15 13:44:34 -0500

    testing

--- efs.tf
+++ efs.tf
@@ -2,10 +2,12 @@ resource "aws_efs_file_system" "moodle-efs" {
   creation_token = "moodle-${random_id.id.hex}"
 }
 
-resource "aws_efs_mount_target" "moodle-mnt-target" {
+resource "aws_efs_mount_target" "moodle-mnt-target-subnet1" {
   file_system_id = aws_efs_file_system.moodle-efs.id
-  subnet_id = join(",", [
-    "${aws_subnet.tstapp-subnet1.id}",
-    "${aws_subnet.tstapp-subnet2.id}"
-  ])
+  subnet_id = "${aws_subnet.tstapp-subnet1.id}"
+}
+
+resource "aws_efs_mount_target" "moodle-mnt-target-subnet2" {
+  file_system_id = aws_efs_file_system.moodle-efs.id
+  subnet_id = "${aws_subnet.tstapp-subnet2.id}"
 }

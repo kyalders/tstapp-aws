252d646a48f4e479ffdfe2463ff7982b084cee78
kyalders <kyle.alderson91@gmail.com>
2023-01-15 13:38:59 -0500

    testing

--- efs.tf
+++ efs.tf
@@ -4,8 +4,8 @@ resource "aws_efs_file_system" "moodle-efs" {
 
 resource "aws_efs_mount_target" "moodle-mnt-target" {
   file_system_id = aws_efs_file_system.moodle-efs.id
-  subnet_id      = [
+  subnet_id = join(",", [
     "${aws_subnet.tstapp-subnet1.id}",
     "${aws_subnet.tstapp-subnet2.id}"
-  ]
+  ])
 }

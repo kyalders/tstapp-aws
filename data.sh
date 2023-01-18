c27a6562aee37d875abbfcdce2c8e40dbed6a32e
kyalders <kyle.alderson91@gmail.com>
2023-01-18 16:10:05 -0500

    modifying asg

--- data.sh
+++ data.sh
@@ -20,11 +20,11 @@ sudo yum install -y amazon-efs-utils
 #sudo python3 -m pip install botocore
 sudo mkdir -p /var/www/moodledata /var/www/html/moodle
 mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${moodleefs}:/ /var/www/moodledata
-sudo chown apache:apache /var/www/moodledata /var/www/html/moodle
 sudo git clone https://github.com/moodle/moodle.git /var/www/html/moodle
 
 # Add if RDS has been created within last 30 minutes statement? 
 sudo php /var/www/html/moodle/admin/cli/install.php --wwwroot=http://${moodlealb}/moodle --dataroot=/var/www/moodledata/ --dbtype=mysqli --dbhost=${moodledbhost} --dbname=${moodledbname} --dbuser=${moodledbuser} --dbpass=${moodledbpw} --dbport=3306 --fullname=moodle --shortname=moodle --adminpass=${moodledbpw} --non-interactive --agree-license --allow-unstable
-
+sudo chown -R apache:apache /var/www/moodledata /var/www/html/moodle
+sudo chmod -R 700 /var/www/html/moodle
 sudo systemctl start httpd
 sudo systemctl enable httpd
